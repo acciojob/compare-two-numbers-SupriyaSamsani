e1e5714dc8b944d39ddaa8596c0456cc6e9929bf
SupriyaSamsani <142519317+SupriyaSamsani@users.noreply.github.com>
2023-10-03 19:18:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function comparenumbers(a,b) {
+	if (a==b) {
+		return true;
+	} else {
+		return false;
+	}
+}
+const a = prompt("Enter First Number:");
+const b = prompt("Enter Second Number:");
+alert(comparenumbers(a,b));
